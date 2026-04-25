34c291e00c8fc9b1fb163d3389fd3138d4a5ecfa
zohaasad <bscs24135@itu.edu.pk>
2026-04-25 18:58:05 +0500

    change made

--- src/sds.h
+++ src/sds.h
@@ -7,3 +7,9 @@ struct sds_header {
     size_t cap;   
     char   data[]; 
 };
+
+typedef char* sds;
+
+inline sds_header* sds_get_header(sds s) {
+    return reinterpret_cast<sds_header*>(s - sizeof(sds_header));
+}
