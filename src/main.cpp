d7952eafd8be4dad0b2d12f49fca3c9a3f42c831
Fizzah-Amir <bscs24031@itu.edu.pk>
2026-04-26 17:07:26 +0500

    main  completed

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include "server.h"
+#include "commands.h"
+#include"unistd.h"
+static void print_usage(const char* prog) {
+    printf("Usage: %s [--port <port>]\n", prog);
+    printf("  --port  port to listen on (default 6380)\n");
+}
+
+int main(int argc, char* argv[]) {
+    int port = 6380;
+
+    // parse arguments
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "--port") == 0 && i + 1 < argc) {
+            port = atoi(argv[++i]);
+        } else if (strcmp(argv[i], "--help") == 0) {
+            print_usage(argv[0]);
+            return 0;
+        }
+    }
+
+    // initialise the global dictionary (Person B's code)
+    commands_init();
+
+    // initialise and run the server
+    Server s;
+    if (!server_init(&s, port)) {
+        fprintf(stderr, "[minired] failed to start\n");
+        return 1;
+    }
+
+    server_run(&s);   // blocks forever
+    return 0;
+}
