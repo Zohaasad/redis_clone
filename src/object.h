3573e8ea58b75420019d5aa227335f509f7c81a6
zohaasad <bscs24135@itu.edu.pk>
2026-04-26 20:37:52 +0500

    added

--- src/object.h
+++ src/object.h
@@ -7,3 +7,21 @@ typedef enum {
     OBJ_HASH   = 2,
     OBJ_ZSET   = 3
 } ObjType;
+struct List;
+struct HashTable;
+struct SortedSet;
+
+struct Obj {
+    ObjType  type;
+    int64_t  expire_at_ms;  
+
+    union {
+        char*       str;    
+        List*       list;    
+        HashTable*  hash;
+        SortedSet*  zset;  
+    };
+
+    explicit Obj(char* s) : type(OBJ_STRING), expire_at_ms(0), str(s) {}
+    Obj() : type(OBJ_STRING), expire_at_ms(0), str(nullptr) {}
+};
