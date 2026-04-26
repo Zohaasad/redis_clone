5af76b5c640469db468899f355dc6656b32261cb
zohaasad <bscs24135@itu.edu.pk>
2026-04-26 21:04:27 +0500

    added

--- src/commands.h
+++ src/commands.h
@@ -3,7 +3,9 @@
 #include <vector>
 #include <string>
 #include "dict.h"
+using namespace std;
 extern Dict* g_dict;
 struct Client;
 void commands_init();
 
+void dispatch_command(Client* client, vector<string>& args);
