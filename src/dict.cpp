9242a3eeff21e288904d776a7b5d987d77275861
zohaasad <bscs24135@itu.edu.pk>
2026-04-26 20:41:26 +0500

    add

--- src/dict.cpp
+++ src/dict.cpp
@@ -3,3 +3,12 @@
 #include <cstring>
 #define DICT_INITIAL_SIZE 16
 #define DICT_LOAD_FACTOR  0.75
+
+static size_t dict_hash(const char* key, size_t klen) {
+    size_t hash = 14695981039346656037ULL;
+    for (size_t i = 0; i < klen; i++) {
+        hash ^= (unsigned char)key[i];
+        hash *= 1099511628211ULL;
+    }
+    return hash;
+}
